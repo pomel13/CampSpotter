3dee9af1a71fe3a992610404ee8c0e3094b48a9c
pomel13 <pomelnikovandrey13@gmail.com>
2026-03-14 16:42:47 +0300

    feat: implement authentication system
    - implement user registration service
    - implement login endpoint
    - integrate Spring Security authentication manager
    - implement CustomUserDetailsService
    - configure password encoder

---
+++ backend/src/main/java/ru/campspotter/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package ru.campspotter.backend.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.campspotter.backend.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        ru.campspotter.backend.model.entity.User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
---
+++ backend/src/main/java/ru/campspotter/backend/service/AuthService.java
@@ -0,0 +1,61 @@
+package ru.campspotter.backend.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.campspotter.backend.model.dto.auth.AuthResponse;
+import ru.campspotter.backend.model.dto.auth.LoginRequest;
+import ru.campspotter.backend.model.dto.auth.RegisterRequest;
+import ru.campspotter.backend.model.dto.user.UserResponseDTO;
+import ru.campspotter.backend.model.entity.User;
+import ru.campspotter.backend.model.mapper.UserMapper;
+import ru.campspotter.backend.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public UserResponseDTO register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        String hashedPassword = passwordEncoder.encode(request.getPassword());
+
+        User user = new User(
+                request.getEmail(),
+                hashedPassword,
+                request.getUsername()
+        );
+
+        User savedUser = userRepository.save(user);
+
+        return UserMapper.toDTO(savedUser);
+    }
+
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        return  new AuthResponse("LOGIN_SUCCESS");
+    }
+}
